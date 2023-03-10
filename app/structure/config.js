d189a6094fb78803f36cb38e169fb7863a288506
d21927790d <dedepankeke@gmail.com>
2023-03-10 02:41:19 +0100

    Generacion de mapa + Some aux functions.

--- app/structure/config.js
+++ app/structure/config.js
@@ -8,11 +8,19 @@ function randInt(min, max) {
 //=============================================================================
 // JUGADOR 
 //=============================================================================
-class jugador {
-    constructor(id, orden, baraja) {
-        this.id     = id;    // Id del usuario.  
-        this.orden  = orden; // Su turno para jugar (1->4).
-        this.baraja = baraja; // Baraja del jugador:
+function generar_id() {
+    var symbols = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var id = '';
+    for (var i = 0; i < 8; i++) {
+        id += symbols.charAt(Math.floor(Math.random() * symbols.length));
+    }
+    return id; 
+}
+
+class Player {
+    constructor() { 
+        this.order = -1;   // Su turno para jugar (1->4).
+        this.deck  = null; // Baraja del jugador:
                               //  - [0]: Total de madera.
                               //  - [1]: Total de trigo.
                               //  - [2]: Total de oveja.
@@ -29,49 +37,51 @@ class jugador {
     }
 }
 
-//=============================================================================
-// TABLERO 
-//=============================================================================
-class figura {
-    constructor(id, tipo) {
-        this.id = id;     // Id del jugador al que pertenece.
-        this.tipo = tipo; // Tipo de figura. 0:Pueblo, 1:Ciudad.
+function create_player(id) {
+    return new Player(id);
+}
+
+var nplayers = 0;
+var players  = {};
+function add_player() {
+    if (nplayers < 4) {
+        let id = generar_id();
+        players[id] = create_player(id);
+        let list = document.getElementById('tabla-jugadores').getElementsByTagName('tbody')[0];
+        let row  = list.insertRow();
+        let cell = row.insertCell(0);
+        cell.innerHTML = id;
+        cell = row.insertCell(1);
+        cell.innerHTML = 'Orden...';
+        console.log(players);
+        nplayers++;
     }
 }
 
-class Biome {
-    constructor(type) {
-        this.type   = type; // Tipo del bioma. 0:Bosque, 1:Cultivo, 2:Campo, 3:Montaña, 4:Terreno, 5:Desierto.
-        this.number = 0;    // Numero del bioma: 1-12. Si es desierto, no tiene (0).
+// La idea es que reciba el id, tire los dados en el backend y 
+// guarde la informacion del id.
+function simular_seleccion_orden() {0
+    let tiradas = [0,0,0,0];
+    for (let i = 0; i < 4; i++) {
+        tiradas[i] = tirar_dados();
     }
-};
+    console.log(tiradas);
+    players.sort(function(a,b) {
+        let idxA = tiradas.indexOf(jugadores.indexOf(b));
+        let idxB = tiradas.indexOf(jugadores.indexOf(a));
 
-class Node {
-    constructor(x, y, biomas) {
-        this.x      = x;      // Coordenadas del nodo.
-        this.y      = y;
-        this.biomas = biomas; // Biomas colindantes al nodo.
-        this.figura = null;   // Figura que hay construida si hay.   
-    }
-};
+        if (tiradas[idxA] === tiradas[idxB]) {
+            return Math.round(Math.random()) * 2 - 1;
+        } else {
+            return tiradas[idxB] - tiradas[idxA];
+        }
+    });
+    console.log(players)
+}
 
-class Path {
-    constructor(points) {
-        this.points   = points; // Nodos entre los que esta la arista.
-        this.has_road = 0;      // Carretera construida. 0:No, 1:Si.
-    }
-};
-
-class tablero {
-    constructor(biomas, nodos, caminos) {
-        this.biomas = biomas;   // Hexagonos (biomas) del tablero.
-        this.bioma_ladron = -1; // Bioma en el que esta el ladron.
-        this.nodos = nodos;     // Vertices (nodos) del tablero.
-        this.caminos = caminos; // Aristas (caminos) del tablero.
-        this.figuras = [];      // Coordenadas de las figuras construidas.
-    }
-};
-
+//=============================================================================
+// TABLERO 
+//=============================================================================
 // Biomas repartidos:
 // 1. Tierra de cultivo/Grano   4
 // 2. Bosque/Leña               4
@@ -89,12 +99,50 @@ const Tokens_start_pos = { 0:0, 1:2, 2:4, 3:6, 4:8, 5:10 }
 // los dados:          A  B  C  D  E  F   G  H   I   J  K  L   M  N  O  P  Q  R
 const Number_tokens = [5, 2, 6, 3, 8, 10, 9, 12, 11, 4, 8, 10, 9, 4, 5, 6, 3, 11];
 
-const Coords_limits = [2,3,3,4,4,5,5,4,4,3,3,2];
+const Borders = [[3,7],[2,8],[2,8],[1,9],[1,9],[0,10],[0,10],[1,9],[1,9],[2,8],[2,8],[3,7]];
+//const YCoorBorders = [];
 
-// Pido perdon por tremenda monstruosidad pero no se me ocurre una manera
-// algoritmica de generar el tablero, es una fumada la generacion de mapas
-// hexagonales.
-function generar_tablero() {
+//class figura {
+//    constructor(id, tipo) {
+//        this.id = id;     // Id del jugador al que pertenece.
+//        this.tipo = tipo; // Tipo de figura. 0:Pueblo, 1:Ciudad.
+//    }
+//}
+
+class Biome {
+    constructor(type) {
+        this.type   = type; // Tipo del bioma. 0:Bosque, 1:Cultivo, 2:Campo, 3:Montaña, 4:Terreno, 5:Desierto.
+        this.number = 0;    // Numero del bioma: 1-12. Si es desierto, no tiene (0).
+    }
+};
+
+class Node {
+    constructor(x, y, biomas) {
+        this.x      = x;      // Coordenada x del nodo.
+        this.y      = y;      // Coordenada y del nodo.
+        this.biomas = biomas; // Biomas colindantes al nodo.
+        this.figura = null;   // Figura que hay construida si hay.   
+    }
+};
+
+class Path {
+    constructor(points) {
+        this.points   = points; // Nodos entre los que esta la arista.
+        this.has_road = 0;      // Carretera construida. 0:No, 1:Si.
+    }
+};
+
+class Board {
+    constructor(biomes, nodes, paths) {
+        this.biomes = biomes;    // Hexagonos (biomas) del tablero.
+        this.thief_biome = null; // Bioma en el que esta el ladron.
+        this.nodes = nodes;      // Vertices (nodos) del tablero.
+        this.paths = paths;      // Aristas (caminos) del tablero.
+        this.figures = [];       // Coordenadas de las figuras construidas.
+    }
+};
+
+function generate_board() {
 
     // Generar biomas:
     let terraform = Biome_terraform;
@@ -109,7 +157,7 @@ function generar_tablero() {
 
     // Generar fichas númericas:
     let tokens_start = Tokens_start_pos[randInt(0,6)];
-    console.log("INICIO DE LOS TOKENS. BIOMA: ", tokens_start);
+    //console.log("INICIO DE LOS TOKENS. BIOMA: ", tokens_start);
     for (let i = 0; i < 18; i++) {
         let j = (i+tokens_start)%19;
         if (biomes[j].type == Biome_id.Desert) {
@@ -119,119 +167,127 @@ function generar_tablero() {
         biomes[j].number = Number_tokens[i]
     }
 
-    // Para comprobar:
-    for (let i = 0; i < 19; i++) {
-        console.log("Bioma ", i, ": ", Biome_name[biomes[i].type], ", numerito: ", biomes[i].number)
-    }
-
-    // Generar nodos: NO SIRVE.
+    // Generar nodos:
     var nodes = {
-        00: new Node(0,0,[biomes[0]]),
-        01: new Node(0,1,[biomes[1]]), 
-        02: new Node(0,2,[biomes[2]]),
-        10: new Node(1,0,[biomes[0]]),
-        11: new Node(1,1,[biomes[0], biomes[1]]),
-        12: new Node(1,2,[biomes[1], biomes[2]]),
-        13: new Node(1,3,[biomes[2]]),
-        20: new Node(2,0,[biomes[0], biomes[11]]),
-        21: new Node(2,1,[biomes[0], biomes[1], biomes[12]]),
-        22: new Node(2,2,[biomes[1], biomes[2], biomes[13]]),
-        23: new Node(2,3,[biomes[2], biomes[3]]),
-        30: new Node(3,0,[biomes[11]]),
-        31: new Node(3,1,[biomes[0], biomes[11], biomes[12]]),
-        32: new Node(3,2,[biomes[1], biomes[12], biomes[13]]),
-        33: new Node(3,3,[biomes[2], biomes[13], biomes[3]]),
-        34: new Node(3,4,[biomes[3]]),
-        40: new Node(4,0,[biomes[11], biomes[10]]),
-        41: new Node(4,1,[biomes[11], biomes[12], biomes[18]]),
-        42: new Node(4,2,[biomes[12], biomes[13], biomes[19]]),
-        43: new Node(4,3,[biomes[13], biomes[3], biomes[14]]),
-        44: new Node(4,4,[biomes[3], biomes[4]]),
-        50: new Node(5,0,[biomes[10]]),
-        51: new Node(5,1,[biomes[11], biomes[10], biomes[18]]),
-        52: new Node(5,2,[biomes[12], biomes[18], biomes[19]]),
-        53: new Node(5,3,[biomes[13], biomes[19], biomes[14]]),
-        54: new Node(5,4,[biomes[3], biomes[14], biomes[4]]),
-        55: new Node(5,5,[biomes[4]]),
-        60: new Node(6,0,[biomes[10]]),
-        61: new Node(6,1,[biomes[10], biomes[18], biomes[9]]),
-        62: new Node(6,2,[biomes[18], biomes[19], biomes[16]]),
-        63: new Node(6,3,[biomes[19], biomes[14], biomes[15]]),
-        64: new Node(6,4,[biomes[14], biomes[4], biomes[5]]),
-        65: new Node(6,5,[biomes[4]]),
-        70: new Node(7,0,[biomes[10], biomes[9]]),
-        71: new Node(7,1,[biomes[18], biomes[9], biomes[16]]),
-        72: new Node(7,2,[biomes[19], biomes[16], biomes[15]]),
-        73: new Node(7,3,[biomes[14], biomes[15], biomes[5]]),
-        74: new Node(7,4,[biomes[4], biomes[5]]),
-        80: new Node(8,0,[biomes[9]]),
-        81: new Node(8,1,[biomes[9], biomes[16], biomes[8]]),
-        82: new Node(8,2,[biomes[16], biomes[15], biomes[7]]),
-        83: new Node(8,3,[biomes[15], biomes[5], biomes[6]]),
-        84: new Node(8,4,[biomes[5]]),
-        90: new Node(9,0,[biomes[9], biomes[8]]),
-        91: new Node(9,1,[biomes[16], biomes[8], biomes[7]]),
-        92: new Node(9,2,[biomes[15], biomes[7], biomes[6]]),
-        93: new Node(9,3,[biomes[5], biomes[6]]),
-        100: new Node(10,0,[biomes[8]]),
-        101: new Node(10,1,[biomes[8], biomes[7]]),
-        102: new Node(10,2,[biomes[7], biomes[6]]),
-        103: new Node(10,3,[biomes[6]]),
-        110: new Node(11,0,[biomes[8]]),
-        111: new Node(11,1,[biomes[7]]),
-        112: new Node(11,2,[biomes[6]])
-    };
+        03 : new Node(0,3,[biomes[0]]),
+        05 : new Node(0,5,[biomes[1]]),
+        07 : new Node(0,7,[biomes[2]]),
+        12 : new Node(1,2,[biomes[0]]),
+        14 : new Node(1,4,[biomes[0],biomes[1]]),
+        16 : new Node(1,6,[biomes[1],biomes[2]]),
+        18 : new Node(1,8,[biomes[2]]),
+        22 : new Node(2,2,[biomes[0],biomes[11]]),
+        24 : new Node(2,4,[biomes[0],biomes[1],biomes[12]]),
+        26 : new Node(2,6,[biomes[1],biomes[2],biomes[13]]),
+        28 : new Node(2,8,[biomes[2],biomes[3]]),
+        31 : new Node(3,1,[biomes[11]]),
+        33 : new Node(3,3,[biomes[0],biomes[11],biomes[12]]),
+        35 : new Node(3,5,[biomes[1],biomes[12],biomes[13]]),
+        37 : new Node(3,7,[biomes[2],biomes[13],biomes[3]]),
+        39 : new Node(3,9,[biomes[3]]),
+        41 : new Node(4,1,[biomes[11],biomes[10]]),
+        43 : new Node(4,3,[biomes[11],biomes[12],biomes[17]]),
+        45 : new Node(4,5,[biomes[12],biomes[13],biomes[18]]),
+        47 : new Node(4,7,[biomes[13],biomes[3],biomes[14]]),
+        49 : new Node(4,9,[biomes[3],biomes[4]]),
+        50 : new Node(5,0,[biomes[10]]),
+        52 : new Node(5,2,[biomes[11],biomes[10],biomes[17]]),
+        54 : new Node(5,4,[biomes[12],biomes[17],biomes[18]]),
+        56 : new Node(5,6,[biomes[13],biomes[18],biomes[14]]),
+        58 : new Node(5,8,[biomes[3],biomes[14],biomes[4]]),
+        510: new Node(5,10,[biomes[4]]),
+        60 : new Node(6,0,[biomes[10]]),
+        62 : new Node(6,2,[biomes[10],biomes[17],biomes[9]]),
+        64 : new Node(6,4,[biomes[17],biomes[18],biomes[16]]),
+        66 : new Node(6,6,[biomes[18],biomes[14],biomes[15]]),
+        68 : new Node(6,8,[biomes[14],biomes[4],biomes[5]]),
+        610: new Node(6,10,[biomes[4]]),
+        71 : new Node(7,1,[biomes[10],biomes[9]]),
+        73 : new Node(7,3,[biomes[17],biomes[9],biomes[16]]),
+        75 : new Node(7,5,[biomes[18],biomes[16],biomes[15]]),
+        77 : new Node(7,7,[biomes[14],biomes[15],biomes[5]]),
+        79 : new Node(7,9,[biomes[4],biomes[5]]),
+        81 : new Node(8,1,[biomes[9]]),
+        83 : new Node(8,3,[biomes[9],biomes[16],biomes[8]]),
+        85 : new Node(8,6,[biomes[16],biomes[15],biomes[7]]),
+        87 : new Node(8,7,[biomes[15],biomes[5],biomes[6]]),
+        89 : new Node(8,9,[biomes[5]]),
+        92 : new Node(9,2,[biomes[9],biomes[8]]),
+        94 : new Node(9,4,[biomes[16],biomes[8],biomes[7]]),
+        96 : new Node(9,6,[biomes[15],biomes[7],biomes[6]]),
+        98 : new Node(9,8,[biomes[5],biomes[6]]),
+        102: new Node(10,2,[biomes[8]]),
+        104: new Node(10,4,[biomes[8],biomes[7]]),
+        106: new Node(10,6,[biomes[7],biomes[6]]),
+        108: new Node(10,8,[biomes[6]]),
+        113: new Node(11,3,[biomes[8]]),
+        115: new Node(11,5,[biomes[7]]),
+        117: new Node(11,7,[biomes[6]])
+    }
 
-    var paths = {}
-    // Para x par: sea (x,y) comprobar (x-1,y), (x+1,y), (x+1,y+1).
-    // Para x impar: sea (x,y) comprobar (x-1,y-1), (x-1,y), (x+1,y). 
-    for (let key in nodes) {
-        let key_x, key_y, x = nodes[key].x, y = nodes[key].y;
-        if (x%2 == 0) {
-            if (x-1 >= 0) {
-                key_x = (x-1).toString();
-                key_y = y.toString();
-                if (!((key_x+key_y) in paths)) {
-                    paths[path_key] = new Path([nodes[key_x], nodes[key_y]]);
+    // Para x = par (2n). (x,y) -> (x-1,y),(x+1,y-1),(x+1,y+1)
+    // Para x = impar (2n+1). (x,y) -> (x-1,y-1),(x-1,y+1),(x+1,y)
+    var paths = {};
+    for (var ncoor in nodes) {
+        n = nodes[ncoor];
+        if (n.x % 2 == 0) {
+            if (n.x-1 > 0) {
+                let m = (n.x-1).toString() + (n.y).toString();
+                if (!(ncoor+m in nodes) && !(m+ncoor in nodes)) {
+                    paths[ncoor+m] = new Path([n,nodes[m]]);
                 }
             }
-
+            if (n.y-1 >= Borders[n.x+1][0]) {
+                let m = (n.x+1).toString() + (n.y-1).toString();
+                if (!(ncoor+m in nodes) && !(m+ncoor in nodes)) {
+                    paths[ncoor+m] = new Path([n,nodes[m]]);
+                }
+            }
+            if (n.y+1 <= Borders[n.x+1][1]) {
+                let m = (n.x+1).toString() + (n.y+1).toString();
+                if (!(ncoor+m in nodes) && !(m+ncoor in nodes)) {
+                    paths[ncoor+m] = new Path([n,nodes[m]]);
+                }
+            }
         } else {
-
-        }
-    }
-
-    /*
-    var nodos = {};
-    for (let i = 0; i <= 11; i++) {
-        for (let j = 0; j <= 12; j++) {
-            var coords = i.toString() + j.toString();
-            var biomeList = [];
-            if (i == 0) {
-                biomeList.push(biomes[0]);
-            } else if (i == 11) {
-                biomeList.push(biomes[8]);
-            } else {
-                if (j == 0) {
-                    biomeList.push(biomes[i]);
-                } else if (j == 12) {
-                    biomeList.push(biomes[i+5]);
-                } else {
-                    biomeList.push(biomes[i]);
-                    biomeList.push(biomes[i+1]);
+            if (n.y-1 >= Borders[n.x-1][0]) {
+                let m = (n.x-1).toString() + (n.y-1).toString();
+                if (!(ncoor+m in nodes) && !(m+ncoor in nodes)) {
+                    paths[ncoor+m] = new Path([n,nodes[m]]);
+                } 
+            }
+            if (n.y+1 <= Borders[n.x-1][1]) {
+                let m = (n.x-1).toString() + (n.y+1).toString();
+                if (!(ncoor+m in nodes) && !(m+ncoor in nodes)) {
+                    paths[ncoor+m] = new Path([n,nodes[m]]);
                 }
             }
-            nodos[coords] = new Node(biomeList);
-        }
-    }*/
-    for (var key in nodos) {
-        if (nodos.hasOwnProperty(key)) {
-            var nodo = nodos[key];
-            console.log(key, nodo);
+            if (n.x+1 < 12) {
+                let m = (n.x+1).toString() + (n.y).toString();
+                if (!(ncoor+m in nodes) && !(m+ncoor in nodes)) {
+                    paths[ncoor+m] = new Path([n,nodes[m]]);
+                }
+            }
         }
     }
-    return tablero;
+
+    // RAEL: comprueba que el mapa se genera correctamente:
+    //for (let i = 0; i < 19; i++) {
+    //    console.log("Bioma ", i, ": ", Biome_name[biomes[i].type], ", numerito: ", biomes[i].number)
+    //}
+    //for (let p in paths) {
+    //    console.log("Path: ", p, paths[p]);
+    //}
+
+    return new Board(biomes, nodes, paths);
 };
+
+function tirar_dados() {
+    let fst = randInt(1,6); let snd = randInt(1,6);
+    document.getElementById('primer-dado').value = fst;
+    document.getElementById('segundo-dado').value = snd;
+    return fst + snd;
+}
 
 
 //=============================================================================
--- test.cpp
+++ test.cpp
@@ -1,23 +1,79 @@
+#include <iostream>
+#include <map>
+#include <random>
+#include <string> 
+#include <vector>
 
 
-
-
-void generate_map(int edge_size) {
-    
+// 1. Tierra de cultivo/Grano   4
+// 2. Bosque/Leña               4
+// 3. Colinas/Ladrillos         3
+// 4. Montañas/Mineral-Roca     3
+// 5. Pasto/Lana                4
+// 6. Desierto                  1
+const std::vector<int> Biome_terraform {4, 4, 3, 3, 4, 1};
+enum Biome { Farmland, Forest, Hill, Mountain, Pasture, Desert };
+struct Hex {
+    int r;
+    //int q;
+    // s = -q-r
+    Biome type;
+};
+std::ostream& operator<<(std::ostream& os, Biome b) {
+    if (b == Farmland) {
+        return os << "Farmland";
+    } else if (b == Forest) {
+        return os << "Forest";
+    } else if (b == Hill) {
+        return os << "Hill";
+    } else if (b == Mountain) {
+        return os << "Mountain";
+    } else if (b == Pasture) {
+        return os << "Pasture";
+    } else if (b == Desert) {
+        return os << "Desert";
+    } else {
+        return os << "WHAT THE FUCK IS GOING ON.";
+    }
 }
+
+std::map<std::string, Hex> the_map;
+std::random_device rd; // dispositivo de generación de números aleatorios
+std::mt19937 gen(rd()); // generador de números aleatorios utilizando rd como semilla
+std::uniform_int_distribution<> dis(0, 5);
+
+void generate_map(int size) {
+    auto terraform = Biome_terraform;
+    for (int r = 0; r < size; r++) {
+        for (int q = -(size-1); q < size - r; q++) {
+            the_map[std::to_string(r) + std::to_string(q)] = {r, q, Farmland};
+        }
+    }
+    for (int r = 0; r < size; r++) {
+        for (int q = (-(size-1) + r); q < size; q++) {
+            the_map[std::to_string(-r) + std::to_string(q)] = {-r, q, Farmland};
+        }
+    }
+
+    for (auto& it : the_map) {
+        auto& hex = it.second;
+        int type = dis(gen);
+        while (terraform[type] == 0) {
+            type = dis(gen);
+        }
+        terraform[type]--;
+        hex.type = (Biome)type;
+    }
+}
+
 
 int main () {
-    int i = 0 -> 11;
-    int j = 0->2
-            0->3
-            0->3
-            0->4
-            0->4
-            0->5
-            0->5
-            0->4
-            0->4
-            0->3
-            0->3
-            0->2
+    generate_map(3);
+    for (auto& h : the_map) {
+        auto hex = h.second;
+        std::cout << "R: " << hex.r << ", Q: " << hex.q << ", Biome: " << hex.type << "\n";
+    }
+    std::cout << the_map.size() << "\n";
 }
+
+// g++ -std=c++11 test.cpp -o main && .\main.exe
