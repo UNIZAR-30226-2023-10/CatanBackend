a0e98edaf87e7113fcbcb453d897e1cd5a83a411
Jaime <jaimeyoldi12@gmail.com>
2023-03-13 19:17:09 +0100

    funcionamiento conexiones

--- src/routes/game.routes.js
+++ src/routes/game.routes.js
@@ -7,31 +7,3 @@ GameRouter.use('create', Game.create)//crea partida y genera un codigo
 GameRouter.use('join' Game.join)
 module.exports = GameRouter
 
-
-/register
-/login
-/game/create
-/game/join
-
-
-
-conn - [socketIO]
-jugador -[Jugador]
-
-llamada a crear partida(incluye primera tirada)
-
-back -> guarda cambios, envia partidaActualizada a todos y envia las notificaciones
-
-juagador envia -> acccion, idPartida
-
-back -> coge Partida de la BD y se envia al modulo de juego(accion, partida, jugador)
-
-modulo juego devuelve -> (err, partidaActualizada, notify)
-
-notify {
-    juagador1 : [String]
-    jugador2: [String]
-    jugador3: [String]
-}
-
-back -> guarda cambios, envia partidaActualizada a todos y envia las notificaciones
